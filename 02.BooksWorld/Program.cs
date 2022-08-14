990e7bba8f26b14e9d7b925f889c3ffe09249eaf
hasankr <hasankr@abv.bg>
2022-08-14 17:54:43 +0300

    added info

---
+++ 02.BooksWorld/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02_BooksWorld
+{
+    internal class P02_BooksWorld
+    {
+        static void Main(string[] args)
+        {
+            var oldGeners = new List<string>(Console.ReadLine().Split(" | "));
+
+            string command = Console.ReadLine();
+
+            while (command != "Stop!")
+            {
+                string[] tokens = command.Split(" ");
+                string action = tokens[0];
+
+                switch (action)
+                {
+                    case "Join":
+                        string genreJoin = tokens[1];
+
+                        if (!oldGeners.Contains(genreJoin))
+                        {
+                            oldGeners.Add(genreJoin);
+                        }
+                        break;
+                    case "Drop":
+                        string genreDrop = tokens[1];
+
+                        if (oldGeners.Contains(genreDrop))
+                        {
+                            oldGeners.Remove(genreDrop);
+                        }
+                        break;
+                    case "Replace":
+                        string oldGenre = tokens[1];
+                        string newGenre = tokens[2];
+
+                        if (oldGeners.Contains(oldGenre) && !oldGeners.Contains(newGenre))
+                        {
+                            int genreIndex = oldGeners.IndexOf(oldGenre);
+                            oldGeners.Remove(oldGenre);
+                            oldGeners.Insert(genreIndex, newGenre);
+                        }
+                        break;
+                    case "Prefer":
+                        int firstIndex = int.Parse(tokens[1]);
+                        int secondIndex = int.Parse(tokens[2]);
+
+                        if (firstIndex > 0 && firstIndex < oldGeners.Count && secondIndex > 0 && secondIndex < oldGeners.Count)
+                        {
+                            string firstGenre = oldGeners[firstIndex];
+                            string secondGenre = oldGeners[secondIndex];
+
+                            oldGeners.RemoveAt(firstIndex);
+                            oldGeners.Insert(firstIndex, secondGenre);
+                            oldGeners.RemoveAt(secondIndex);
+                            oldGeners.Insert(secondIndex, firstGenre);
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", oldGeners));
+        }
+    }
+}
---
+++ 05.Messages/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pushes = int.Parse(Console.ReadLine());
+            string sms = string.Empty;
+
+            for (int i = 0; i < pushes; i++)
+            {
+                string nums = Console.ReadLine();
+
+                int numLength = nums.Length;         //length
+                                                     // char oneNum = nums[0];
+
+                int firstNum = nums[0] - '0';        //digit
+                int ofset = (firstNum - 2) * 3;      //offset
+
+                if (firstNum == 0)                   //check for space
+                {
+                    sms += (char)(firstNum + 32);    //letter = (char)(firstNum + 32);
+                    continue;                        //removes [
+                }
+
+                if (firstNum == 8 || firstNum == 9)
+                {
+                    ofset++; //ofset = (firstNum - 2) * 3 + 1; 
+                }
+
+                int letterIndex = ofset + numLength - 1;
+
+                //int letter = (char)(letterIndex + 97);  //removed, we need letter not number
+                //int letterCode = letterIndex + 97;
+
+                sms += (char)(letterIndex + 97);         //add letter to message
+            }
+
+            Console.WriteLine(sms);                  //print message
+        }
+    }
+}
